2f425b2c2bd00350d49193ef448278c7a2a57b43
bau04 <120693660+bau04@users.noreply.github.com>
2023-06-30 16:02:49 +0800

    Update main.py

--- Two-digit lottery system/main.py
+++ Two-digit lottery system/main.py
@@ -9,26 +9,20 @@ n2 = int(input("Enter second number: "))
 x1 = int(random.randint(0,9))
 x2 = int(random.randint(0,9))
 
-if n1 == x1 & n2 == x2:
+if n1 == x1 and n2 == x2:
     print("\nToday's lottery result is: ")
     print("Winning number: " , x1 , " , " , x2)
-    print("     Your pick: " , n1 , " , " , n2)
-    print("        Result: You Won!!!")
+    print("Your pick: " , n1 , " , " , n2)
+    print("Result: You Won!!!")
 
-if n1 <= -1 | n2 <= -1 :
+elif n1 <= -1 or n2 <= -1 :
     print("Error: The number you picked is below the range")
 
-if n1 >= 10 | n2 >= 10 :
+elif n1 >= 10 or n2 >= 10 :
     print("Error: The number you picked is above the range ")
 
-elif n1 != x1 & n2 != x2:
+elif n1 != x1 or n2 != x2:
     print("\nToday's lottery result is: ")
     print("Winning number: " , x1 , " , " , x2)
-    print("     Your pick: " , n1 , " , " , n2)
-    print("        Result: You lost")
-
-else :
-    print("\nToday's lottery result is: ")
-    print("Winning number: " , x1 , " , " , x2)
-    print("     Your pick: " , n1 , " , " , n2)
-    print("        Result: You lost")
+    print("Your pick: " , n1 , " , " , n2)
+    print("Result: You lost")
