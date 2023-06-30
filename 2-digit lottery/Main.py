dc148eda11512bf9a30c4ae483ff16ac36669438
bau04 <120693660+bau04@users.noreply.github.com>
2023-06-30 15:08:52 +0800

    Update main.py

--- 2-digit lottery/Main.py
+++ 2-digit lottery/Main.py
@@ -1,6 +1,6 @@
 import random
 
-print("Welcome to the 2-digit lotto draw")
+print("Welcome to the 2-digit lottery draw")
 print("Choose a number from 0-9")
 
 n1 = int(input("Enter first number: "))
@@ -11,24 +11,24 @@ x2 = int(random.randint(0,9))
 
 if n1 == x1 & n2 == x2:
     print("\nToday's lottery result is: ")
-    print("Result: You won!")
-    print("Your pick: " , n1 , " , " , n2)
     print("Winning number: " , x1 , " , " , x2)
+    print("Your pick: " , n1 , " , " , n2)
+    print("Result: You Won!!!")
 
 if n1 <= -1 | n2 <= -1 :
-    print("Error: The number you picked is below 0")
+    print("Error: The number you picked is below the range")
 
 if n1 >= 10 | n2 >= 10 :
-    print("Error: The number you picked is above the range of 0-9")
+    print("Error: The number you picked is above the range ")
 
 elif n1 != x1 & n2 != x2:
     print("\nToday's lottery result is: ")
-    print("Result: You lost")
-    print("Your pick: " , n1 , " , " , n2)
     print("Winning number: " , x1 , " , " , x2)
+    print("Your pick: " , n1 , " , " , n2)
+    print("Result: You lost")
 
 else :
     print("\nToday's lottery result is: ")
-    print("Result: lost")
-    print("Your pick: " , n1 , " , " , n2)
     print("Winning number: " , x1 , " , " , x2)
+    print("Your pick: " , n1 , " , " , n2)
+    print("Result: You lost")
